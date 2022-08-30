f18baaf0fce8b619e449590467022fb9f5673111
Deekonda Narender <deekondanarender@Deekondas-MacBook-Air.local>
2022-08-30 22:35:10 +0100

    Added steps

--- src/test/reboot.feature
+++ src/test/reboot.feature
@@ -1,17 +1,19 @@
-Feature: Reboot
-Background: 
-When I navigate to the device dashboard
+Feature: RebootTests rebooting of devices
+Background:
+Given I navigate to the device dashboard
 
 Scenario: A device tile updates accordingly when rebooting a device
-And I click the Reboot  button of the device at the address  "10.198.162.2"
-Then the device status indicator becomes green
+When I click the Reboot button of the device at the address "10.198.162.2"
+Then The device status indicator becomes green
 
+Scenario Outline: A device tile updates accordingly when rebooting a device
+When I click the Reboot button of the device at the address "<address>"
+Then The device status indicator becomes green
 
-Scenario Outline: All device tile updates accordingly when rebooting that devices
-And I click the Reboot  button of the device at the address  "<address>"
-Then the device status indicator becomes green
-Examples: 
-   |address|
-   |10.198.162.1|
-   |10.198.162.2|
-   |10.198.162.3|
+Examples:
+|address     |
+|10.198.162.1|
+|10.198.162.2|
+|10.198.162.3|
+
+ 
--- src/test/steps.js
+++ src/test/steps.js
@@ -1,6 +1,6 @@
 'use strict';
 require('chromedriver');
-const { Builder, By, window} = require('selenium-webdriver');
+const { Builder, By, window, document, delay} = require('selenium-webdriver');
 const { Before, After, When, Then } = require ('@cucumber/cucumber');
 const { assert } = require('console');
 const until = require('selenium-webdriver/lib/until');
@@ -12,15 +12,47 @@ Before(async function () {
         .build();
 });
 After(async function () {
-    if (!driver) {
+   if (!driver) {
         return;
-    }
-    await driver.quit();
+     }
+     await driver.close();
 });
 
-When('I navigate to the device dashboard', async function() {
+Given('I navigate to the device dashboard', async function() {
 driver.get('http://localhost');
 });
+
+When('I click the Reboot button of the device at the address {string}', async function(deviceAddress) {
+    if(deviceAddress =='10.198.162.1'){
+    //driver.wait.until(until.elementIsVisible(By.css('#app > div > div:nth-child(1) > div > button')))
+        await driver.findElement(By.css('#app > div > div:nth-child(1) > div > button')).click();
+    //await delay(5000);
+    }
+    else if(deviceAddress =='10.198.162.2'){
+    //driver.wait.until(until.elementIsVisible(By.css('#app > div > div:nth-child(2) > div > button')))
+        await driver.findElement(By.css('#app > div > div:nth-child(2) > div > button')).click();
+    //await delay(5000);
+    }
+    else if(deviceAddress =='10.198.162.3'){
+    //driver.wait.until(until.elementIsVisible(By.css('#app > div > div:nth-child(3) > div > button')))
+        await driver.findElement(By.css('#app > div > div:nth-child(3) > div > button')).click();
+    //await delay(5000);
+    }
+    else{
+        console.log('no device selected')
+    }
+    //await driver.findElement(By.css('div.device-info-item>button')).click;
+    //return deviceAddress
+});
+
+Then('The device status indicator becomes green', async function() {
+    //driver.wait(until.elementLocated(By.css('div.device-info-item-offline')));
+    driver.wait(until.elementLocated(By.css('div.device-info-item-online')));
+    //let atttibute = await driver.findElement(By.xpath("(//div[@class='device-info-item-online'])[1]")).getAttribute('value')
+    console.log('device sucessfully rebooted')
+});
+
+/*
 
 When('I click the Reboot button of the device at the address {string}', async function(deviceAddress) {
     await driver.findElement(By.css('div.device-info-item>button')).click;
@@ -34,3 +66,4 @@ Then('The device status indicator becomes green', async function() {
     //let atttibute = await driver.findElement(By.xpath("(//div[@class='device-info-item-online'])[1]")).getAttribute('value')
     console.log('device sucessfully rebooted')
 });
+*/
